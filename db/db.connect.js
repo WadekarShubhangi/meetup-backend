28a5f287ed65bde2b2022c2115af255a99ea4ca4
WadekarShubhangi <shubhangiwadekar1096@gmail.com>
2025-06-04 11:35:13 +0530

    feat: changes db code

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,13 +1,35 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const mongoUri = process.env.MONGODB;
+
+// const initializeDatabase = async () => {
+//   await mongoose
+//     .connect(mongoUri)
+//     .then(() => console.log("Connected to database."))
+//     .catch((error) => console.log("error while connecting to database", error));
+// };
+
+// module.exports = { initializeDatabase };
+
+
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const mongoUri = process.env.MONGODB;
-
+const mongoURI = process.env.MONGODB;
 const initializeDatabase = async () => {
-  await mongoose
-    .connect(mongoUri)
-    .then(() => console.log("Connected to database."))
-    .catch((error) => console.log("error while connecting to database", error));
+  try {
+    const connection = await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    if (connection) {
+      console.log("connected succesfully");
+    }
+  } catch (error) {
+    console.log("connection failed", error);
+  }
 };
 
 module.exports = { initializeDatabase };
